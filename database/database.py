7f4d17ee9f18b6334639da48a766aa72a86618ec
m777ks <m777ks@gmail.com>
2024-11-30 00:24:13 +0400

    v1.0

---
+++ database/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from config_data.config import DATABASE_URL_asyncpg
+
+engine = create_async_engine(
+    url=DATABASE_URL_asyncpg(),
+    echo=True
+)
+
+session_factory = async_sessionmaker(engine)
+
+class Base(DeclarativeBase):
+    pass
